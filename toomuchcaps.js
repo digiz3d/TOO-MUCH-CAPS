d38aa2926975cb6ae46fcb11fb956f089a106910
kekos91 <kekos91@free.fr>
2017-10-29 21:57:18 +0100

    edited script so it finally works

--- toomuchcaps.js
+++ toomuchcaps.js
@@ -1,13 +1,55 @@
-document.addEventListener("scroll", lowerCaseEverything, false);
-
-
-function lowerCaseEverything() {
-    document.querySelectorAll("#video-title").forEach( function(value, index, listObj ) {
-        //value.style.border = "5px solid green";
-        value.style.textTransform = "lowercase";
-    });
+function debug(text) {
+    //console.log(text);
+    return false;
 }
 
 
-//document.getElementById("video-title").style.border = "5px solid red";
-console.log("AHH");
+function lowerCaseSidebar() {
+    debug("+++++ lowerCaseSidebar()");
+    document.querySelectorAll("#video-title").forEach(function(value, index, listObj) {
+        lowerCaseThis(value);
+    });
+}
+
+function lowerCaseVideoTitles() {
+    debug("+++++ lowerCaseVideoTitles()");
+    document.querySelectorAll("#video-title, .title.ytd-video-primary-info-renderer").forEach(function(value, index, listObj) {
+        lowerCaseThis(value);
+    });
+}
+
+function lowerCaseThis(element) {
+    element.style.textTransform = "lowercase";
+}
+
+
+window.addEventListener("yt-page-data-updated", function() {
+    debug("yt-page-data-updated");
+    lowerCaseVideoTitles();
+});
+window.addEventListener("shown-items-changed", function() {
+    debug("shown-items-changed");
+});
+window.addEventListener('load', function() {
+    debug("load");
+    lowerCaseVideoTitles();
+}, true);
+
+window.addEventListener('yt-action', function() {
+    debug("yt-action");
+    lowerCaseVideoTitles();
+}, true);
+window.addEventListener('yt-load-next-continuation', function() {
+    debug("yt-load-next-continuation");
+    lowerCaseVideoTitles();
+}, true);
+window.addEventListener('yt-load-reload-continuation', function() {
+    debug("yt-load-reload-continuation");
+    lowerCaseVideoTitles();
+}, true);
+window.addEventListener('shown-items-changed', function() {
+    debug("shown-items-changed 2");
+    lowerCaseVideoTitles();
+}, true);
+
+debug("TOO MUCH CAPS loaded.");
