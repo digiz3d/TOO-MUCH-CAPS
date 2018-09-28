addbc085917ab5b8b94be6b1c969a066a5b77213
Léandre Daumont <1338620+digiz3d@users.noreply.github.com>
2018-09-28 22:12:40 +0200

    the simpler the better :baby:

--- toomuchcaps.js
+++ toomuchcaps.js
@@ -1,47 +1,12 @@
-function debug(text) {
-    //console.log(text);
-    return false;
+function injectCSS() {
+    let head = document.head || document.getElementsByTagName('head')[0];
+    let css = '* { text-transform: lowercase !important; }';
+    let style = document.createElement('style');
+    style.type = 'text/css';
+    style.appendChild(document.createTextNode(css));
+    head.appendChild(style);
 }
 
-function lowerCaseVideoTitles() {
-    debug("+++++ lowerCaseVideoTitles()");
-    document.querySelectorAll("#video-title, .title.ytd-video-primary-info-renderer, .ytp-title-link.yt-uix-sessionlink, .ytp-videowall-still-info-title, .ytp-upnext-title").forEach(function(value, index, listObj) {
-        lowerCaseThis(value);
-    });
-}
-
-function lowerCaseThis(element) {
-    element.style.textTransform = "lowercase";
-}
-
-
-window.addEventListener("yt-page-data-updated", function() {
-    debug("yt-page-data-updated");
-    lowerCaseVideoTitles();
-});
-window.addEventListener("shown-items-changed", function() {
-    debug("shown-items-changed");
-});
-window.addEventListener('load', function() {
-    debug("load");
-    lowerCaseVideoTitles();
+window.addEventListener('load', function () {
+    injectCSS();
 }, true);
-
-window.addEventListener('yt-action', function() {
-    debug("yt-action");
-    lowerCaseVideoTitles();
-}, true);
-window.addEventListener('yt-load-next-continuation', function() {
-    debug("yt-load-next-continuation");
-    lowerCaseVideoTitles();
-}, true);
-window.addEventListener('yt-load-reload-continuation', function() {
-    debug("yt-load-reload-continuation");
-    lowerCaseVideoTitles();
-}, true);
-window.addEventListener('shown-items-changed', function() {
-    debug("shown-items-changed 2");
-    lowerCaseVideoTitles();
-}, true);
-
-debug("TOO MUCH CAPS loaded.");
