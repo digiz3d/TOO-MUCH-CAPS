e2b7935f5e4a30e8af1a24414d9c349dd12a0d4f
digiz3d <1338620+digiz3d@users.noreply.github.com>
2019-08-26 23:28:46 +0200

    new selectors, new options page. just better

--- toomuchcaps.js
+++ toomuchcaps.js
@@ -1,12 +1,24 @@
-function injectCSS() {
-    let head = document.head || document.getElementsByTagName('head')[0];
-    let css = '* { text-transform: lowercase !important; }';
-    let style = document.createElement('style');
+function injectCSS(item) {
+    var ytbTextTransformStyle = "lowercase";
+    if (item.ytbTextTransformStyle) {
+        ytbTextTransformStyle = item.ytbTextTransformStyle;
+    }
+    var head = document.head || document.getElementsByTagName('head')[0];
+    var css = '#video-title,\
+    .title.style-scope.ytd-video-primary-info-renderer,\
+    .ytp-title-link.yt-uix-sessionlink.ytp-title-fullerscreen-link,\
+    .miniplayer-title.style-scope.ytd-miniplayer { text-transform: ' + ytbTextTransformStyle + ' !important; }';
+    var style = document.createElement('style');
     style.type = 'text/css';
     style.appendChild(document.createTextNode(css));
     head.appendChild(style);
 }
 
 window.addEventListener('load', function () {
-    injectCSS();
+    var getting = browser.storage.sync.get("ytbTextTransformStyle");
+    getting.then(injectCSS, onError);
 }, true);
+
+function onError(error) {
+    console.log(`Error: ${error}`);
+}
