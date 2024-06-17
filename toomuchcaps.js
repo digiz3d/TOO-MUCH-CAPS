10a7830a98300cdb78a1f699179f68f573e34645
unknown <phozon@protonmail.com>
2024-06-17 15:08:14 +0200

    added css selector for videos titles in subscription list aswell

--- toomuchcaps.js
+++ toomuchcaps.js
@@ -1,8 +1,16 @@
 function injectCSS(item) {
     console.log("Injecting CSS with style:", item.ytbTextTransformStyle);
     var ytbTextTransformStyle = item.ytbTextTransformStyle || "lowercase"; // Default to lowercase if undefined
+    
+    // Define CSS selectors and corresponding text-transform style
     var css = `
         .style-scope.ytd-watch-metadata yt-formatted-string { 
+            text-transform: ${ytbTextTransformStyle} !important; 
+        }
+        h3 a yt-formatted-string,
+        .title.style-scope.ytd-video-primary-info-renderer,
+        .ytp-title-link.yt-uix-sessionlink.ytp-title-fullerscreen-link,
+        .miniplayer-title.style-scope.ytd-miniplayer { 
             text-transform: ${ytbTextTransformStyle} !important; 
         }
     `;
@@ -11,8 +19,14 @@ function injectCSS(item) {
     style.type = 'text/css';
     style.appendChild(document.createTextNode(css));
 
-    // Append style to document element to ensure it applies correctly
-    document.documentElement.appendChild(style);
+    // Remove any existing style elements with the same data attribute to avoid duplication
+    document.querySelectorAll('style[data-extension-style="toomuchcaps"]').forEach(e => e.remove());
+
+    // Set a data attribute to identify extension's style
+    style.setAttribute('data-extension-style', 'toomuchcaps');
+    
+    // Append style to document head to ensure it applies correctly
+    document.head.appendChild(style);
 }
 
 function onError(error) {
