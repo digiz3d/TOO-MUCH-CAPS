f8976e70195b6eaf77878230cb9f1232609e613a
phozon <phozon@protonmail.com>
2024-06-18 21:04:18 +0200

    fixing changes requested from main git owner

--- toomuchcaps.js
+++ toomuchcaps.js
@@ -1,5 +1,4 @@
 function injectCSS(item) {
-    console.log("Injecting CSS with style:", item.ytbTextTransformStyle);
     var ytbTextTransformStyle = item.ytbTextTransformStyle || "lowercase"; // Default to lowercase if undefined
     var css = `
         .style-scope.ytd-watch-metadata yt-formatted-string, 
@@ -24,8 +23,6 @@ function injectCSS(item) {
 function onError(error) {
     console.error(`Error in injecting CSS: ${error}`);
 }
-
-console.log("Content script loaded");
 
 // Ensure content script runs when document is ready
 document.addEventListener('DOMContentLoaded', function () {
